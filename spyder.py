dd78fe7fd863ca45773e55d1122ea27fc2f5f1a8
Batiste Bieler <batiste.bieler@gmail.com>
2014-03-01 17:40:45 +0100

    Fix a couple of stuff

--- spyder.py
+++ spyder.py
@@ -6,12 +6,14 @@ import urllib2
 import httplib
 import socket
 import logging
+import pdb
 
 def walk(node):
     yield node
     if hasattr(node, 'childNodes'):
         for n in node.childNodes:
             for m in walk(n):
+                
                 yield m
 
 def node_is(node, name):
@@ -23,14 +25,13 @@ class Scraper(object):
         self.uri = uri
         self.domain = urlparse.urlparse(uri)[1]
         self.http_root = "http://%s" % self.domain
-        self.start_url = self.uri[len(self.http_root):]
         self.all_urls = []
         self.visited_urls = []
         self.urls_to_visit = []
         self.html_urls = []
         self.exclude = exclude
 
-    def get_relative_url(self, current_url, node):
+    def get_absolute_url(self, current_url, node):
 
         if current_url.startswith("/"):
             current_url = current_url[1:]
@@ -38,7 +39,8 @@ class Scraper(object):
         href = node.attributes.get('href', None)
         if href is None:
             return
-        href = href.strip()
+            
+        href = href.value.strip()
         if len(href) == 0:
             href = current_url
 
@@ -64,15 +66,13 @@ class Scraper(object):
         if not href.startswith("/"):
             return current_url + href
 
-        if not href.startswith("/"):
-            return "/" + href
-
         return href
 
     def cleanup_href(self, href):
         # Normalize URLs even more. Eg:
         # directory/toto/../tata/" -> directory/tata/
-        url = urlparse.urlparse(href).path
+        parse = urlparse.urlparse(href)
+        url = parse.path
         new_bits = []
         for bits in url.split('/'):
             if bits=='..' and new_bits:
@@ -80,16 +80,22 @@ class Scraper(object):
             else:
                 new_bits.append(bits)
 
-        return "/".join(new_bits)
-
+        url = "/".join(new_bits)
+        if parse.query:
+            return url + '?' + parse.query
+        return url
+            
     def fetch(self, url):
+        total = self.http_root + url
+        if not url.startswith('/'):
+            logging.warn("Invalid URL %s on ?" % total)
         try:
-            fh = urllib2.urlopen(self.http_root + '/' + url, None, 3)
+            fh = urllib2.urlopen(total, None, 3)
             content_type = fh.info()['content-type']
             content = fh.read()
 
         except (urllib2.URLError, httplib.HTTPException, socket.timeout), err:
-            logging.warn("Cannot fetch url:%s %s" % (self.http_root + '/' + url, err))
+            logging.warn("Cannot fetch url:%s %s" % (total, err))
             return "error", ""
 
         return content_type, content
@@ -106,6 +112,7 @@ class Scraper(object):
         if url in self.urls_to_visit:
             self.urls_to_visit.remove(url)
         try:
+            logging.info("Fetching url:%s" % url)
             content_type, content = self.fetch(url)
         except (urllib2.HTTPError), err:
             logging.warn("Cannot fetch url:%s %s" % (self.http_root + '/' + url, err))
@@ -117,19 +124,24 @@ class Scraper(object):
         else:
             return self.new_content(url, content_type, content)
 
-        parser = html5lib.HTMLParser()
+        from html5lib import treebuilders
+        parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
         dom_tree = parser.parse(content)
         for node in walk(dom_tree):
-            if node.type == 5 and node.name == 'a':
-                href = self.get_relative_url(url, node)
+            if node.nodeType == 1 and node.nodeName == 'a':
+                parse_result = urlparse.urlparse(url)
+                href = self.get_absolute_url(parse_result.path, node)
+                query = urlparse.parse_qs(parse_result.query)
                 if href:
                     href = self.cleanup_href(href)
                     if href not in self.all_urls:
                         self.all_urls.append(href)
+                        if query:
+                            print query
                         self.urls_to_visit.append(href)
 
-    def scrap(self):
-        result = self.scrap_page(self.start_url)
+    def scrap(self, start='/'):
+        result = self.scrap_page(start)
         while len(self.urls_to_visit):
             for url in self.urls_to_visit:
                 self.scrap_page(url)
@@ -139,11 +151,11 @@ class Scraper(object):
     def new_page(self, url, content):
         print url
     
-    def new_content(self, url, content_type):
+    def new_content(self, url, content_type, content):
         print url, content_type
    
 # example
-#def __main__():
-sp = Scraper("http://www.microsoft.com", exclude=["/dont/not/want/"])
-print sp.scrap()
+if __name__ == "__main__":
+    sp = Scraper("http://www.microsoft.com", exclude=["/dont/not/want/"])
+    print sp.scrap()
 
